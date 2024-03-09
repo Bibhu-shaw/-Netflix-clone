833d90d26e7f0f879af53f7475dc18356f1807de
Bibhu-shaw <118754564+Bibhu-shaw@users.noreply.github.com>
2024-03-09 23:11:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openSignupModal() {
+    document.getElementById('signupModal').style.display = 'flex';
+}
+
+function closeSignupModal() {
+    document.getElementById('signupModal').style.display = 'none';
+}
